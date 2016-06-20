7a006893e906e483e00c0081f5ac6aba8568a52e
okandiyebiri <okansibut@gmail.com>
2016-06-20 09:51:47 +0300

    responsive

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html lang="tr">
 	<head>
-	<title>Sayaçlı İndirme Butonu</title>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Sayaçlı İndirme Butonu</title>
 		<link rel="stylesheet" href="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/css/bootstrap.css">
 	</head>
 	<body>
